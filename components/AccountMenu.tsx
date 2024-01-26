2d8fabd0be2c01fa74bf48c389788b9f382d37db
Tong Lin <wowawawoa@gmail.com>
2024-01-26 18:29:14 +1030

    feat: adding info modal

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { signOut } from "next-auth/react";
 import React from "react";
 
@@ -6,6 +7,8 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data } = useCurrentUser();
+
   if (!visible) {
     return null;
   }
@@ -14,13 +17,20 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex ">
       <div className="flex flex-col gap-3">
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
-          <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
+          <img
+            className="w-8 rounded-md"
+            src="/images/default-blue.png"
+            alt=""
+          />
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
-        <hr className="bg-gray-600 border-0 h-px my-4"/>
-        <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
+        <hr className="bg-gray-600 border-0 h-px my-4" />
+        <div
+          onClick={() => signOut()}
+          className="px-3 text-center text-white text-sm hover:underline"
+        >
           Sign out
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
+import InfoModal from "@/components/InfoModal";
 
 import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -28,9 +30,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
